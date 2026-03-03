fd0c05900c3dc7ad78adf639e220e5999b0ba72a
Dev Ashish <ashishudupa282000@gmail.com>
2026-03-03 22:38:46 +0530

    added editing and delete functionality

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 function ToDoItem(props){
+    const [isEditing,setIsEditing] = useState(false);
+    const [editText,setEditText] = useState(props.todo.text)
+
+
+    const handleSave = () => {
+        props.onEdit(props.todo.id, editText);
+        setIsEditing(false);
+      };
     return (
-        <h1>TodoItem</h1>
+        <div className="todo-item">
+            {isEditing ? (
+                <>
+                <input value={editText} onChange={(e)=>setEditText(e.target.value)}/>
+                <button onClick={handleSave}>Save</button>
+                </>
+            ):
+            (
+                <>
+                    <span onClick={() => props.onToggle(props.todo.id)} className="todo-text">
+                        {console.log(props.todo.completed)}
+                        {props.todo.text}
+                    </span>
+                    <div className="actions">
+                        <button onClick={() => setIsEditing(true)}>Edit</button>
+                        <button onClick={() => props.onDelete(props.todo.id)}>Delete</button>
+                    </div>
+                </>
+            )}
+        </div>
     )
 }
 export default ToDoItem;
